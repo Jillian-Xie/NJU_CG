6ac9c50ba5753b5177529c05a6e5cca0a22d3986
Jillian <xie.ljing@gmail.com>
2020-10-03 21:01:46 +0800

    Trying to implement polygon

---
+++ cg_algorithms.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 本文件只允许依赖math库
+import math
+
+
+def draw_line(p_list, algorithm):
+    """绘制线段
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 线段的起点和终点坐标
+    :param algorithm: (string) 绘制使用的算法，包括'DDA'和'Bresenham'，此处的'Naive'仅作为示例，测试时不会出现
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    x0, y0 = p_list[0]
+    x1, y1 = p_list[1]
+
+    if x0 > x1:
+        x0, y0, x1, y1 = x1, y1, x0, y0
+
+    result = []
+    dx = x1 - x0
+    dy = y1 - y0
+
+    if algorithm == 'Naive':
+        if x0 == x1:
+            for y in range(y0, y1 + 1):
+                result.append((x0, y))
+        else:
+            k = dy / dx
+            for x in range(x0, x1 + 1):
+                result.append((x, int(y0 + k * (x - x0))))
+
+    elif algorithm == 'DDA':
+        if x0 == x1:
+            for y in range(min(y0, y1), max(y0, y1) + 1):
+                result.append((x0, y))
+        elif y0 == y1:
+            for x in range(x0, x1 + 1):
+                result.append((x, y0))
+        elif dx == dy:
+            for x in range(min(x0, x1), max(x0, x1) + 1):
+                result.append((x, x - x0 + y0))
+        elif dx == -dy:
+            for x in range(x0, x1 + 1):
+                result.append((x, -x + x0 + y0))
+        else:
+            k = dy / dx
+            if abs(dx) > abs(dy):
+                x = x0
+                y = y0
+                result.append((x, y))
+                for i in range(abs(dx) + 1):
+                    x = x + 1
+                    y = y + k
+                    result.append((x, round(y)))
+            else:
+                if y0 > y1:
+                    x0, y0, x1, y1 = x1, y1, x0, y0
+                m = 1 / k
+                x = x0
+                y = y0
+                result.append((x, y))
+                for i in range(abs(dy) + 1):
+                    x = x + m
+                    y = y + 1
+                    result.append((round(x), y))
+
+    elif algorithm == 'Bresenham':
+        if x0 == x1:
+            for y in range(min(y0, y1), max(y0, y1) + 1):
+                result.append((x0, y))
+        elif y0 == y1:
+            for x in range(x0, x1 + 1):
+                result.append((x, y0))
+        elif dx == dy:
+            for x in range(x0, x1 + 1):
+                result.append((x, x - x0 + y0))
+        elif dx == -dy:
+            for x in range(x0, x1 + 1):
+                result.append((x, -x + x0 + y0))
+        else:
+            k = dy / dx
+            x = x0
+            y = y0
+            if 0 < k < 1:
+                d = dx - 2 * dy
+                for i in range(dx + 1):
+                    result.append((int(x), int(y)))
+                    x = x + 1
+                    if d < 0:
+                        y = y + 1
+                        d = d + 2 * dx - 2 * dy
+                    else:
+                        d = d - 2 * dy
+            elif -1 < k < 0:
+                dy = abs(dy)
+                d = dx - 2 * dy
+                for i in range(dx + 1):
+                    result.append((int(x), int(y)))
+                    x = x + 1
+                    if d < 0:
+                        y = y - 1
+                        d = d + 2 * dx - 2 * dy
+                    else:
+                        d = d - 2 * dy
+            elif k > 1:
+                d = dy - 2 * dx
+                for i in range(dy + 1):
+                    result.append((int(x), int(y)))
+                    y = y + 1
+                    if d < 0:
+                        x = x + 1
+                        d = d + 2 * dy - 2 * dx
+                    else:
+                        d = d - 2 * dx
+            elif k < -1:
+                dy = abs(dy)
+                d = dy - 2 * dx
+                for i in range(dy + 1):
+                    result.append((int(x), int(y)))
+                    y = y - 1
+                    if d < 0:
+                        x = x + 1
+                        d = d + 2 * dy - 2 * dx
+                    else:
+                        d = d - 2 * dx
+
+    return result
+
+
+def draw_polygon(p_list, algorithm):
+    """绘制多边形
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 多边形的顶点坐标列表
+    :param algorithm: (string) 绘制使用的算法，包括'DDA'和'Bresenham'
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    result = []
+    for i in range(len(p_list)):
+        line = draw_line([p_list[i - 1], p_list[i]], algorithm)
+        result += line
+    return result
+
+
+def draw_ellipse(p_list):
+    """绘制椭圆（采用中点圆生成算法）
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 椭圆的矩形包围框左上角和右下角顶点坐标
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    pass
+
+
+def draw_curve(p_list, algorithm):
+    """绘制曲线
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 曲线的控制点坐标列表
+    :param algorithm: (string) 绘制使用的算法，包括'Bezier'和'B-spline'（三次均匀B样条曲线，曲线不必经过首末控制点）
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 绘制结果的像素点坐标列表
+    """
+    pass
+
+
+def translate(p_list, dx, dy):
+    """平移变换
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param dx: (int) 水平方向平移量
+    :param dy: (int) 垂直方向平移量
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    pass
+
+
+def rotate(p_list, x, y, r):
+    """旋转变换（除椭圆外）
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param x: (int) 旋转中心x坐标
+    :param y: (int) 旋转中心y坐标
+    :param r: (int) 顺时针旋转角度（°）
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    pass
+
+
+def scale(p_list, x, y, s):
+    """缩放变换
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1], [x2, y2], ...]) 图元参数
+    :param x: (int) 缩放中心x坐标
+    :param y: (int) 缩放中心y坐标
+    :param s: (float) 缩放倍数
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1], [x_2, y_2], ...]) 变换后的图元参数
+    """
+    pass
+
+
+def clip(p_list, x_min, y_min, x_max, y_max, algorithm):
+    """线段裁剪
+
+    :param p_list: (list of list of int: [[x0, y0], [x1, y1]]) 线段的起点和终点坐标
+    :param x_min: 裁剪窗口左上角x坐标
+    :param y_min: 裁剪窗口左上角y坐标
+    :param x_max: 裁剪窗口右下角x坐标
+    :param y_max: 裁剪窗口右下角y坐标
+    :param algorithm: (string) 使用的裁剪算法，包括'Cohen-Sutherland'和'Liang-Barsky'
+    :return: (list of list of int: [[x_0, y_0], [x_1, y_1]]) 裁剪后线段的起点和终点坐标
+    """
+    pass
